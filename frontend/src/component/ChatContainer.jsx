236c99cb8a5203ac93e7504af9880a09c5462b18
deeyaShah <diyadineshshah@gmail.com>
2025-06-26 00:10:57 +0530

    some ui changes

--- frontend/src/component/ChatContainer.jsx
+++ frontend/src/component/ChatContainer.jsx
@@ -59,8 +59,8 @@ const ChatContainer = () => {
                 <img
                   src={
                     message.senderId === authUser._id
-                      ? authUser.profilePic || "/avatar.png"
-                      : selectedUser.profilePic || "/avatar.png"
+                      ? authUser.profilePic || "/avatar1.png"
+                      : selectedUser.profilePic || "/avatar1.png"
                   }
                   alt="profile pic"
                 />
